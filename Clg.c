355285445da92676f1f94d1363bea8ad9a6e9585
Abhishek <153516999+JigenxOhtsusuki@users.noreply.github.com>
2024-08-27 14:09:18 +0530

    Update Clg.c

--- Clg.c
+++ Clg.c
@@ -1,10 +1,13 @@
-import java.io.*;
-public class Ifstmt {  
+public class IfElseExample {  
 public static void main(String[] args) {  
-int age=20;  
-if(age>18)
+    int number=13;  
+      if(number%2==0)
 {  
-    System.out.print("Age is greater than 18");  
-            }  
+        System.out.println("even number");  
+    }
+else
+{  
+        System.out.println("odd number");  
+  }  
 }  
 }  
