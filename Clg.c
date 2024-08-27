a9cd05c15db2885b13f0ee0c8c2e815391353a72
Abhishek <153516999+JigenxOhtsusuki@users.noreply.github.com>
2024-08-27 14:25:02 +0530

    Update Clg.c

--- Clg.c
+++ Clg.c
@@ -1,21 +1,44 @@
-import java.util.Scanner;  
-public class LargestNumber  
-{  
-public static void main(String args[])  
-{  
-int num1, num2, num3;  
-System.out.println("Enter three integers: ");  
-Scanner in = new Scanner(System.in);  
-num1=in.nextInt();  
-num2=in.nextInt();  
-num3=in.nextInt();  
-if (num1 > num2 && num1 > num3)  
-System.out.println("The largest number is: "+num1);  
-else if (num2 > num1 && num2 > num3)  
-System.out.println("The largest number is: "+num2);  
-else if (num3 > num1 && num3 > num2)  
-System.out.println("The largest number is: "+num3);  
-else  
-System.out.println("The numbers are same.");  
-}  
-}  
+Import java.io.*;
+import java.util.Scanner; 
+public class Ladderifelse 
+{ 
+public static void main(String[] args)
+ { 
+Scanner scan = new Scanner(System.in); 
+System.out.println("Enter percentage marks"); 
+double percentage = scan.nextDouble();
+ if(percentage >= 90)
+{
+ System.out.println("Excellent: Grade A"); 
+}
+else if (percentage < 90 && percentage >= 80)
+{ 
+System.out.println("Very Good: Grade B"); 
+}
+else if(percentage < 80 && percentage >= 70)
+{
+ System.out.println("Good: Grade C"); 
+}
+else if(percentage < 70 && percentage >= 60)
+{ 
+System.out.println("Satisfactory: Grade D"); 
+
+}else if(percentage < 60 && percentage >= 50)
+
+{
+System.out.println("Work Hard: Grade E"); 
+
+}else if(percentage < 50 && percentage >= 40)
+
+{ 
+
+System.out.println("Just Passed: Grade F"); 
+
+}
+else 
+{ 
+
+System.out.println("Failed!"); 
+}
+} 
+}
