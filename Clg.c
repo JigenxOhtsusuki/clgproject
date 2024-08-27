f7323acd3554f4cfcc1f911e8f592268170f01c6
Abhishek <153516999+JigenxOhtsusuki@users.noreply.github.com>
2024-08-27 14:16:22 +0530

    Update Clg.c

--- Clg.c
+++ Clg.c
@@ -1,13 +1,29 @@
-public class IfElseExample {  
-public static void main(String[] args) {  
-    int number=13;  
-      if(number%2==0)
-{  
-        System.out.println("even number");  
-    }
-else
-{  
-        System.out.println("odd number");  
-  }  
-}  
-}  
+import java.util.Scanner; 
+public class OddEven
+ { 
+public static boolean isOddEven(int n)
+ {
+if(n % 2 == 0) 
+return true; 
+else 
+return false;
+ }
+ public static void main(String[] args) 
+{ 
+ int number = 0;
+ boolean result = false; 
+ Scanner scan = new Scanner(System.in);
+ System.out.print("Enter an integer number: "); 
+ number = scan.nextInt(); 
+ result=isOddEven(number); 
+if(result)
+{
+System.out.println(number+" is an even number"); 
+}
+else 
+{
+System.out.println(number+" is an odd number"); 
+ object scan.close();
+ }
+ }
+}
